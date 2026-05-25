3ea4f54ddad978f7baa7d54412320540e70c4b2c
Fernanda Dimitri <dimitri-dg@hotmail.com>
2026-05-25 13:37:33 -0300

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,17 @@ async function bootstrap() {
   app.useGlobalPipes(
      new ValidationPipe({ 
       whitelist: true, 
-      forbidNonWhitelisted: true, 
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      }
     }) 
   )
 
   app.setGlobalPrefix('api/v2');
 
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`App running on port ${process.env.PORT}`)
 }
 bootstrap();
